76a6018d836a64d98d8afd7a4d8d47f1053a6020
BryanInche <BryanInche>
2024-02-06 23:09:51 +0000

    sa

---
+++ notebooks/exploration.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+#Librerias
+import pandas as pd  # Libreria para administrar tablas, y realizar trabajos con distintas formas de tablas o dataframes
+import numpy as np   # Libreria para poder hacer operaciones matematicas y matriciales
+import matplotlib.pyplot as plt # Libreria para realizar graficos 
