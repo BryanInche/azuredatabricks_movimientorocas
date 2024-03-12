664da3f2748c45340585245d907734db94b00f57
BryanInche <BryanInche>
2024-03-12 22:51:17 +0000

    avance43

--- code/4.model_development.py
+++ code/4.model_development.py
@@ -4,12 +4,10 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Normalization
 from tensorflow.keras.optimizers import Adam
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
 import numpy as np
-import tensorflow as tf
-import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
 
 # 1. Cargamos los datos para construir el Modelo Machine Learning
 df_delta = spark.read.format("delta").load("/mnt/datalakemlopsd4m/processed/datmarcobre_featureengineer_delta")
@@ -24,7 +22,7 @@ X = datos[['capacidad_en_volumen_equipo_carguio_m3',
 'capacidad_en_volumen_equipo_acarreo_m3']].values
 y = datos['numero_pases_carguio'].values # Reemplaza 'variable_objetivo' con el nombre de tu variable objetivo
 
-# 3. Construccion del modelo de RED NEURONAL(LSTM)
+# 3. Construccion del modelo de RED NEURONAL
 # Aseguramos que los resultados sean "reproducibles" en cada ejecucion de tensorflow(pesos iniciales aleatorios)
 np.random.seed(42)
 tf.random.set_seed(42)
@@ -33,14 +31,21 @@ tf.random.set_seed(42)
 X_train_rnn, X_test_rnn, y_train_rnn, y_test_rnn = train_test_split(X, y, test_size=0.2, random_state=42)
 #X_train_rnn, X_test_rnn, y_train_rnn, y_test_rnn = train_test_split(X_array, y_array, test_size=0.2, random_state=42)
 
+#Arquitectura 1:
+# model_rnn = Sequential()
+# # La forma de entrada para LSTM debe ser (n_timesteps, n_features)
+# model_rnn.add(LSTM(30, activation='relu', input_shape=(1, 5)))  #  (n_samples, n_pasos, n_variables)
+# # Agregar capas Dense según sea necesario
+# model_rnn.add(Dense(60, activation='relu'))
+# model_rnn.add(Dense(30, activation='relu'))
+# model_rnn.add(Dense(15, activation='relu'))
+# # Capa de salida
+# model_rnn.add(Dense(1, activation='linear'))
+
+#Arquitectura 2:
 model_rnn = Sequential()
-# La forma de entrada para LSTM debe ser (n_timesteps, n_features)
-model_rnn.add(LSTM(30, activation='relu', input_shape=(1, 5)))  #  (n_samples, n_pasos, n_variables)
-# Agregar capas Dense según sea necesario
-model_rnn.add(Dense(60, activation='relu'))
-model_rnn.add(Dense(30, activation='relu'))
-model_rnn.add(Dense(15, activation='relu'))
-# Capa de salida
+model_rnn.add(Dense(15, activation='relu', input_shape=[5]))
+model_rnn.add(Dense(7, activation='relu'))
 model_rnn.add(Dense(1, activation='linear'))
 
 # Compilar el modelo
@@ -49,7 +54,10 @@ model_rnn.compile(loss='mean_squared_error', optimizer='adam')
 model_rnn.summary()
 
 # Entrenar el modelo
-history = model_rnn.fit(X_train_rnn.reshape(-1, 1, 5), y_train_rnn, epochs=5, validation_data=(X_test_rnn.reshape(-1, 1, 5),y_test_rnn), batch_size=5, verbose=1)
+# Red LSTM
+#history = model_rnn.fit(X_train_rnn.reshape(-1, 1, 5), y_train_rnn, epochs=5, validation_data=(X_test_rnn.reshape(-1, 1, 5),y_test_rnn), batch_size=5, verbose=1)
+# Red Tradicional
+history = model_rnn.fit(X_train_rnn, y_train_rnn, epochs=4, validation_data=(X_test_rnn,y_test_rnn), batch_size=5, verbose=1)
 
 # 4. Validacion del Modelo con la metrica de RSME
 # VALIDACION 1 del Modelo
@@ -62,8 +70,10 @@ train_rmse = np.sqrt(train_loss)
 val_rmse = np.sqrt(val_loss)
 
 # VALIDACION 2 del Modelo
-# Hacer predicciones en el conjunto de prueba
-y_pred_rnn = model_rnn.predict(X_test_rnn.reshape(-1, 1, 5)) # (n_samples, n_pasos, n_variables)
+# Hacer predicciones en el conjunto de prueba Red LSTM
+#y_pred_rnn = model_rnn.predict(X_test_rnn.reshape(-1, 1, 5)) # (n_samples, n_pasos, n_variables)
+# Hacer predicciones en el conjunto de prueba Red Tradicional
+y_pred_rnn = model_rnn.predict(X_test_rnn) # (n_samples, n_pasos, n_variables)
 
 # Redondear los valores de y_pred al entero más cercano
 y_pred_rnn = np.round(y_pred_rnn).astype('int64')
@@ -74,9 +84,13 @@ mse = mean_squared_error(y_test_rnn, y_pred_rnn)
 rmse = np.sqrt(mse)
 print(f'Raíz del Error Cuadrático Medio en el conjunto de prueba: {rmse}')
 
-# 5. Guardar el Modelo ML 
-# Guardando todos los componentes del modelo (arquitectura, pesos, configuración de entrenamiento, estado del optimizador, etc.)
-# SavedModel de TensorFlow
-model_rnn.save("/modelos/model_rnn_pasesv2")
+# 5. Guardar el Modelo ML en Azure Storage
+#5.1. Guardar en el directorio LOCAL (.OS)
+model_rnn.save('/modelornn_version1_d4m/modelornn_version1')
 
+#5.2. Copiar el Modelo ML desde el Directorio LOCAL(.OS)  HASTA  Directorio de DBFS de Azure DataBrinks
+dbutils.fs.cp("file:/modelornn_version1_d4m/modelornn_version1", "dbfs:/modelornn_version1_d4m/modelornn_version1", recurse=True) # True:Copy carpeta, False:Copy archivo
+
+#5.3. Copiar el modelo desde el directorio DBFS (Azure DataBrinks) al contenedor de almacenamiento Azure Storage
+dbutils.fs.cp("/modelornn_version1_d4m/modelornn_version1", "/mnt/datalakemlopsd4m/presentation/ModelsResults/modelornn_version1", recurse=True) #True:Copycarpeta
 
